a463c5b077bd2353bbbab3077616726ac1371711
arnabmitra <arnabmitra.mitra@gmail.com>
2021-12-01 18:06:22 -0700

    day1.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,35 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+    println(partOne(input.map { x-> x.toInt() }))
+    println(partTwo(input.map { x-> x.toInt() }))
+
+}
+
+private fun partOne(input: List<Int>): Int {
+    var prev = 0
+
+    var prev1 = prev
+    var increased = 0
+
+    for ((index, value) in input.withIndex()) {
+        if (index != 0 && value > prev1) {
+            increased++
+        }
+        prev1 = value
+    }
+    return increased
+}
+
+fun partTwo(input : List<Int>): Int {
+    val listOfWindows = mutableListOf<Int>()
+    for (i in 0..(input.size - 3)) {
+        listOfWindows.add(input[i] + input[i + 1] + input[i + 2])
+    }
+    return partOne(listOfWindows)
+
 }
